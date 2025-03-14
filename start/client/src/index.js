6289720110e63484296a28343e83fe6228adae44
ryota_kimura <ddi_00016@dk.daihatsu.co.jp>
2025-03-14 17:21:58 +0900

    フロントエンド部分のコード修正

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,44 +1,62 @@
-import './style.css';
-
-function getApiUrl() {
-
-    const backend = process.env.BACKEND_URL;
-    
-    const url = (backend) ? `${backend}` : ``;
-    return url;
-}
+import "./style.css";
+import { BACKEND_URL } from "./env";
 
 const app = new Vue({
-    el: '#app',
-    interval: null,
-    data() { 
-        return {
-            stocks: []
-        }
-    },
-    methods: {
-        async update() {
-            try {
-                
-                const url = `${getApiUrl()}/api/getStocks`;
-                console.log('Fetching stocks from ', url);
+  el: "#app",
+  data() {
+    return {
+      stocks: [],
+    };
+  },
+  methods: {
+    async getStocks() {
+      try {
+        const url = `${BACKEND_URL}/api/getStocks`;
+        console.log("Fetching stocks from ", url);
 
-                const response = await fetch(url);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-
-                app.stocks = await response.json();
-            } catch (ex) {
-                console.error(ex);
-            }
-        },
-        startPoll() {
-            this.interval = setInterval(this.update, 5000);
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(
+            `HTTP error! status: ${response.status} - ${response.statusText}`
+          );
         }
+        app.stocks = await response.json();
+      } catch (ex) {
+        console.error(ex);
+      }
     },
-    created() {
-        this.update();
-        this.startPoll();
-    }
+  },
+  created() {
+    this.getStocks();
+  },
 });
+
+const connect = () => {
+  const signalR_URL = `${BACKEND_URL}/api`;
+  console.log(`Connecting to SignalR...${signalR_URL}`);
+
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl(signalR_URL)
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+  connection.onclose(() => {
+    console.log("SignalR connection disconnected");
+    setTimeout(() => connect(), 2000);
+  });
+
+  connection.on("updated", (updatedStock) => {
+    console.log("Stock updated message received", updatedStock);
+    const index = app.stocks.findIndex((s) => s.id === updatedStock.id);
+    console.log(
+      `${updatedStock.symbol} Old price: ${app.stocks[index].price}, New price: ${updatedStock.price}`
+    );
+    app.stocks.splice(index, 1, updatedStock);
+  });
+
+  connection.start().then(() => {
+    console.log("SignalR connection established");
+  });
+};
+
+connect();
